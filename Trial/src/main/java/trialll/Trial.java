65df79838b690831ffc107d98b121380215f7191
sonasunny123 <sonasunny023@gmail.com>
2025-06-23 19:34:50 +0530

    commit

---
+++ Trial/src/main/java/trialll/Trial.java
@@ -0,0 +1,14 @@
+package trialll;
+
+public class Trial {
+
+	public static void main(String[] args)
+	{
+		int a=3;
+		int b=4;
+		int r=a+b;
+		System.out.println(a+"+"+b+"="+r);
+
+	}
+
+}
---
+++ Trial/src/main/java/trialll/Trial1.java
@@ -0,0 +1,16 @@
+package trialll;
+
+public class Trial1 {
+
+	public static void main(String[] args) 
+	{
+
+
+		int a=3;
+		int b=4;
+		int r=a-b;
+		System.out.println(a+"+"+b+"="+r);
+
+	}
+
+}
